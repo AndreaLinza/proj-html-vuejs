6815d9209398d3eb2d800ef34da91f46de572a5d
AndreaLinza <andrealinza@gmail.com>
2023-09-04 20:28:40 +0200

    component button

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import ButtonUp from './ButtonUp.vue';
 import TheFooter from './components/TheFooter.vue';
 import TheHeader from './components/TheHeader.vue';
 import TheMain from './components/TheMain.vue';
@@ -7,8 +8,9 @@ export default {
   components: {
     TheHeader,
     TheMain,
-    TheFooter
-  }
+    TheFooter,
+    ButtonUp
+}
 }
 
 
@@ -19,63 +21,12 @@ export default {
   <TheHeader></TheHeader>
   <TheMain></TheMain>
   <TheFooter></TheFooter>
-  <div class="rombo">
-    <div class="btn-bg">
-      <button type="button" class="btn return-btn"><a href="#"><i class="fa-solid fa-angles-up"
-            style="color: black;"></i></a></button>
-    </div>
-  </div>
+  <ButtonUp></ButtonUp>
+  
 </template>
 
 <style lang="scss" scoped>
 @use "../src/styles/partials/mixins" as *;
 
-.rombo {
-  position: fixed;
-  bottom: 20px;
-  right: 15px;
-  width: 45px;
-  height: 45px;
-  -moz-transform: rotate(45deg) skew(10deg, 10deg);
-  -webkit-transform: rotate(45deg) skew(10deg, 10deg);
-  -ms-transform: rotate(45deg) skew(10deg, 10deg);
-  -o-transform: rotate(45deg) skew(10deg, 10deg);
-  transform: rotate(45deg) skew(10deg, 10deg);
-  border: 2px solid $color-secondary;
-}
 
-.btn-bg {
-  position: fixed;
-  bottom: 3px;
-  right: 3px;
-  width: 35px;
-  height: 35px;
-  -moz-transform: rotate(45deg) skew(10deg, 10deg);
-  -webkit-transform: rotate(45deg) skew(10deg, 10deg);
-  -ms-transform: rotate(45deg) skew(10deg, 10deg);
-  -o-transform: rotate(45deg) skew(10deg, 10deg);
-  transform: rotate(0deg) skew(1deg, 1deg);
-  background-color: $color-secondary;
-}
-
-
-button {
-  border-radius: 0;
-
-
-  &:active {
-    border-color: transparent !important;
-
-  }
-
-  a {
-    position: absolute;
-    transform: rotate(315deg);
-    font-size: 20px;
-    top: 0px;
-    right: 10px;
-
-  }
-
-}
 </style>
---
+++ src/ButtonUp.vue
@@ -0,0 +1,71 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+
+<template>
+    <div class="rombo">
+        <div class="btn-bg">
+            <button type="button" class="btn return-btn"><a href="#"><i class="fa-solid fa-angles-up"
+                        style="color: black;"></i></a></button>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use "./styles/partials/mixins" as *;
+
+.rombo {
+    position: fixed;
+    bottom: 20px;
+    right: 15px;
+    width: 45px;
+    height: 45px;
+    -moz-transform: rotate(45deg) skew(10deg, 10deg);
+    -webkit-transform: rotate(45deg) skew(10deg, 10deg);
+    -ms-transform: rotate(45deg) skew(10deg, 10deg);
+    -o-transform: rotate(45deg) skew(10deg, 10deg);
+    transform: rotate(45deg) skew(10deg, 10deg);
+    border: 2px solid $color-secondary;
+}
+
+.btn-bg {
+    position: fixed;
+    bottom: 3px;
+    right: 3px;
+    width: 35px;
+    height: 35px;
+    -moz-transform: rotate(45deg) skew(10deg, 10deg);
+    -webkit-transform: rotate(45deg) skew(10deg, 10deg);
+    -ms-transform: rotate(45deg) skew(10deg, 10deg);
+    -o-transform: rotate(45deg) skew(10deg, 10deg);
+    transform: rotate(0deg) skew(1deg, 1deg);
+    background-color: $color-secondary;
+}
+
+
+button {
+    border-radius: 0;
+
+
+    &:active {
+        border-color: transparent !important;
+
+    }
+
+    a {
+        position: absolute;
+        transform: rotate(315deg);
+        font-size: 20px;
+        top: 0px;
+        right: 10px;
+
+    }
+
+}
+</style>
