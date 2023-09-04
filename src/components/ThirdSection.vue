c09e4d3997323b77608e44abaa0606cb654b82fa
AndreaLinza <andrealinza@gmail.com>
2023-09-04 03:31:44 +0200

    carousel done

--- src/components/ThirdSection.vue
+++ src/components/ThirdSection.vue
@@ -8,7 +8,7 @@ export default {
         return {
 
 
-            currentPage: 0,
+            currentIndex: 0,
             itemsForPage: 4,
             list: [
 
@@ -51,71 +51,71 @@ export default {
                     vote: 2,
                     text_product: 'Labyrinths of th World',
                     first_price: '',
-                    price: '£83.00 - £90.00'
+                    price: '£110.00'
                 },
                 {
                     img_product: 'public/12-327x327.jpg',
-                    discount: "",
+                    discount: "-20%",
                     vote: 2,
-                    text_product: 'Labyrinths of th World',
-                    first_price: '',
-                    price: '£83.00 - £90.00'
+                    text_product: "Reginald's Death Arena",
+                    first_price: '£115.00',
+                    price: '£92.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
-                    discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
-                    first_price: '',
-                    price: '£83.00 - £90.00'
+                    img_product: 'public/13-327x327.jpg',
+                    discount: "-26%",
+                    vote: 3,
+                    text_product: 'Martin Goes on the Attack',
+                    first_price: '£88.00',
+                    price: '£65.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
+                    img_product: 'public/14-327x327.jpg',
                     discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
+                    vote: 0,
+                    text_product: "What's in the Window",
                     first_price: '',
-                    price: '£83.00 - £90.00'
+                    price: '£70.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
-                    discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
-                    first_price: '',
-                    price: '£83.00 - £90.00'
+                    img_product: 'public/15-327x327.jpg',
+                    discount: "-10%",
+                    vote: 0,
+                    text_product: 'Legend of Mana Remastered',
+                    first_price: '£100.00',
+                    price: '£90.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
+                    img_product: 'public/16-327x327.jpg',
                     discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
+                    vote: 3,
+                    text_product: 'Babol the Walking Box',
                     first_price: '',
-                    price: '£83.00 - £90.00'
+                    price: '£75.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
-                    discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
-                    first_price: '',
-                    price: '£83.00 - £90.00'
+                    img_product: 'public/17-327x327.jpg',
+                    discount: "-27%",
+                    vote: 1,
+                    text_product: 'Adam The Storyteller',
+                    first_price: '£96.00',
+                    price: '£70.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
-                    discount: "",
-                    vote: 2,
-                    text_product: 'Labyrinths of th World',
-                    first_price: '',
-                    price: '£83.00 - £90.00'
+                    img_product: 'public/02-327x327.jpg',
+                    discount: "-29%",
+                    vote: 4,
+                    text_product: 'The Little Tank Hunter',
+                    first_price: '£85.00',
+                    price: '£60.00'
                 },
                 {
-                    img_product: 'public/12-327x327.jpg',
-                    discount: "",
+                    img_product: 'public/03-327x327.jpg',
+                    discount: "-20%",
                     vote: 2,
-                    text_product: 'Labyrinths of th World',
+                    text_product: 'Train Station Renovation',
                     first_price: '',
-                    price: '£83.00 - £90.00'
+                    price: '£92.00 - £110.00'
                 },
             ]
 
@@ -123,45 +123,32 @@ export default {
     },
     computed: {
         visibleProduct() {
-            const startIndex = this.currentPage * this.itemsForPage;
+            const startIndex = this.currentIndex;
             const endIndex = startIndex + this.itemsForPage;
             return this.listProduct.slice(startIndex, endIndex);
         }
     },
     methods: {
         prevSlide() {
+            const maxPage = this.listProduct.length -4
 
-            if (this.currentPage > 0) {
-                this.currentPage--;
+            if (this.currentIndex > 0) {
+                this.currentIndex--;
             }
-            else if (this.currentPage = 0) {
-                this.currentPage++
+            else {
+                this.currentIndex = maxPage
             }
         },
         nextSlide() {
-            const maxPage = Math.ceil(this.listProduct.length / this.itemsForPage) - 1;
-            if (this.currentPage < maxPage) {
-                this.currentPage++;
+            const maxPage = this.listProduct.length -4
+            if (this.currentIndex < maxPage) {
+                this.currentIndex++;
             }
-            else if (this.currentPage = maxPage) {
-                this.currentPage = 0
+            else {
+                this.currentIndex = 0
             }
         },
     },
-
-
-
-    // computed: {
-    //     list() {
-    //         return ['Featured', 'New Arrival', 'Best Sellers'];
-    //     },
-    // },
-
-
-
-
-
-
 
     };
 
@@ -170,34 +157,15 @@ export default {
 
 
 <template>
-    
-        <!-- <section>
-            <div class="container">
-                <h2 class="text-center text-light mt-5 fw-bold fs-1">Our Product</h2>
-                <div class="d-flex justify-content-center"><span class="divider"></span></div>
-                <div class="list-group list-group-horizontal">
-                    <a href="#" class="list-group-item" v-for="item in list">{{ item }}</a>
-                </div>
-
-                <carousel :per-page="4">
-                    <slide v-for="(product, index) in listProduct" :key="index">
-                        
-                    </slide>
-                </carousel>
-            </div>
-        </section> -->
-    
-
     <section>
-        <div class="container">
+        <div class="container-fluid">
             <h2 class="text-center text-light mt-5 fw-bold fs-1">Our Product</h2>
             <div class="d-flex justify-content-center"><span class="divider"></span></div>
             <div class="list-group list-group-horizontal">
                 <a href="#" class="list-group-item" v-for="item in list">{{ item }}</a>
 
             </div>
-            <div class="slider-container">
-
+            <div class="slider container">
                 <div class="slider row row-cols-4 g-4 mt-4">
                     <div v-for="(product, i) in visibleProduct" :key="i">
                         <div class="category-card">
